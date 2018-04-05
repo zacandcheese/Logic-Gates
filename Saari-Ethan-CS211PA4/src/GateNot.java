481966dc621de55c3d0961ae66dbdc9b11a6887b
Zach Nowak <zacandcheese@icloud.com>
2018-04-05 10:15:56 -0400

    Summiting GateNot

--- Saari-Ethan-CS211PA4/src/GateNot.java
+++ Saari-Ethan-CS211PA4/src/GateNot.java
@@ -1,7 +1,18 @@
 import java.util.List;
 public class GateNot extends Gate{
 	
-	public GateNot(List<Wire> inputs, Wire output) {
+	public GateNot(Wire inputs, Wire output) {
 		super("NOT",inputs,output);
+	}
+	@Override public boolean propagate() {
+		boolean value = true;
+		Wire newWire = getInputs().get(0);
+		Signal signal = newWire.getSignal();
+		signal = signal.invert();
+		if (getOutput().getSignal().equals(signal)) {
+			value = false;
+		}
+		getOutput().setSignal(signal);
+		return value;
 	}
 }
