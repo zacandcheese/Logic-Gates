ddc6461c8320770b78e7765feb49ddea014cc01f
UrbaNinja3393 <UrbaNinja3393@users.noreply.github.com>
2018-04-05 10:02:01 -0400

    Exception 1
    First exception class

---
+++ Saari-Ethan-CS211PA4/src/ExceptionLogicMalformedSignal.java
@@ -0,0 +1,35 @@
+
+public class ExceptionLogicMalformedSignal {
+//Fields
+	private char bad;
+	private String msg;
+//Methods
+	//public ExceptionLogicMalformedSignal(char bad, String msg) constructor that assigns to each field.
+	public ExceptionLogicMalformedSignal(char bad, String msg) {
+		this.bad = bad;
+		this.msg = msg;
+	}
+	//@Override public String toString() return your msg.
+	@Override public String toString() {
+		return this.msg;
+	}
+	
+//Getters and Setters
+	//Get the bad
+	public char getBad() {
+		return bad;
+	}
+	//Set the bad
+	public void setBad(char bad) {
+		this.bad = bad;
+	}
+	//Get the message
+	public String getMsg() {
+		return msg;
+	}
+	//Set the message
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
