ee15b6fbaae365a5adf554df2c62929cdefade57
Zach Nowak <zacandcheese@icloud.com>
2018-04-05 11:08:37 -0400

    Stuff

--- Saari-Ethan-CS211PA4/src/GateNot.java
+++ Saari-Ethan-CS211PA4/src/GateNot.java
@@ -1,8 +1,15 @@
 import java.util.List;
+import java.util.ArrayList;
 public class GateNot extends Gate{
 	
 	public GateNot(Wire inputs, Wire output) {
-		super("NOT",inputs,output);
+		super("NOT",one(inputs),output);
+	}
+	// Helper method to create an ArrayList of one thing so that Java's // stupid "super() must be first line" rule can be honored 
+	public static <T> ArrayList<T> one(T x){  
+		ArrayList<T> a = new ArrayList<T>();  
+		a.add(x);  
+		return a; 
 	}
 	@Override public boolean propagate() {
 		boolean value = true;
