27dcbadfe811846374cd0f7cc5f60af4ac052e7f
UrbaNinja3393 <UrbaNinja3393@users.noreply.github.com>
2018-04-07 23:03:16 -0400

    SignalFixes

--- Saari-Ethan-CS211PA4/src/GateAnd.java
+++ Saari-Ethan-CS211PA4/src/GateAnd.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class GateAnd extends Gate{
 	
 	public GateAnd(List<Wire> inputs, Wire output) {
-		super("AND", inputs,output);
+		super("AND", inputs, output);
 	}
 	// Helper method to create an ArrayList of one thing so that Java's // stupid "super() must be first line" rule can be honored 
 	public static <T> ArrayList<T> one(T x){  
@@ -14,10 +14,15 @@ public class GateAnd extends Gate{
 	@Override public boolean propagate() {
 		boolean value = true;
 		boolean flag = false;
+		boolean xFlag = false;
 		Signal signal = Signal.X;
 		for(Wire w: getInputs()) {
 			if(w.getSignal() == Signal.LO) {
 				flag = true;
+				break;
+			}
+			else if(w.getSignal() == Signal.X) {
+				xFlag = true;
 				break;
 			}
 			else continue;
@@ -25,6 +30,9 @@ public class GateAnd extends Gate{
 		if(flag) {
 			signal = Signal.LO;
 			//meaning one was negative
+		}
+		else if(xFlag) {
+			signal = Signal.X;
 		}
 		else signal = Signal.HI;
 		
--- Saari-Ethan-CS211PA4/src/GateOr.java
+++ Saari-Ethan-CS211PA4/src/GateOr.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class GateOr extends Gate{
 	
 	public GateOr(List<Wire> inputs, Wire output) {
-		super("OR", inputs,output);
+		super("OR", inputs, output);
 	}
 	// Helper method to create an ArrayList of one thing so that Java's // stupid "super() must be first line" rule can be honored 
 	public static <T> ArrayList<T> one(T x){  
@@ -13,22 +13,30 @@ public class GateOr extends Gate{
 	}
 	//Hi if any inputs are high;LO otherwise
 	@Override public boolean propagate() {
+		Signal signal = Signal.LO;
 		boolean value = true;
 		boolean flag = false;
-		Signal signal = Signal.X;
+		boolean xFlag = false;
 		for(Wire w: getInputs()) {
 			if(w.getSignal() == Signal.HI) {
 				flag = true;
 				break;
 			}
+			else if(w.getSignal() == Signal.X) {
+				xFlag = true;
+				break;
+			}
 			else continue;
 		}
+		
 		if(flag) {
 			signal = Signal.HI;
 			//meaning one was negative
 		}
-		else signal = Signal.LO;
-		
+		else if(xFlag){
+			signal = Signal.X;
+		}
+
 		//Checks to see if changed signal
 		if (getOutput().getSignal().equals(signal)) {
 			value = false;
